ca242677afb39ccc58d37a5285be1aceb7c2813c
Clint Lohr <lohrcl@oregonstate.edu>
2023-03-13 17:53:42 -0600

    Update str_prim_macros.asm

--- str_prim_macros.asm
+++ str_prim_macros.asm
@@ -1,12 +1,6 @@
 ; Author: Clinton Lohr
 ; Last Modified: 12/04/2021	
-; Description: This program takes in ten signed integers from the user. Values are stored
-;	as strings. Each input is validated to make sure it fits into a 32-bit register and is a signed integer.
-;	The integer is then converted from string form into decimal form. Once in decimal form the sum,
-;	and average are calculated and stored in memory. Finally, the stored integers are converted
-;	back into ascii characters and the list of entered integers, the sum, and the average are
-;	displayed. The program uses Macros to gather input and display input. Conversions are done using
-;	string primitives.
+
 
 INCLUDE Irvine32.inc
 
